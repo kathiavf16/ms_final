3f89c2429508d7c59e2058719f80cf01c385f00d
kathiavf16 <kathia.vargasfeliz@gmail.com>
2021-12-20 17:21:29 -0500

    layout progress

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ import { Gauge} from "./Gauge.js";
 import { Sunburst} from "./Sunburst.js";
 import {Sunburst2} from "./Sunburst2.js";
 import {Line} from "./Line.js";
+import {Heatmap} from "./Heatmap.js";
 
 
 let test;
@@ -15,6 +16,7 @@ let sunburst;
 let gauge;
 let sunburst2;
 let line;
+let heatmap;
 
 let state = {
 
@@ -26,6 +28,7 @@ let state = {
   gauge: null,
   sunburst2: null,
   line: null,
+  heatmap: null,
   
 };
 
@@ -33,11 +36,13 @@ Promise.all([
   d3.csv("./data/test.csv", d3.autoType),
   d3.csv("./data/bubble20.csv", d3.autoType),
   d3.csv("./data/sunburst.csv", d3.autoType),
+  d3.csv("./data/greenhouse.csv", d3.autoType),
  
-]).then(([test, bubble, sunburst]) => {
+]).then(([test, bubble, sunburst, greenhouse]) => {
   state.test = test;
   state.bubble = bubble;
   state.sunburst = sunburst;
+  state.greenhouse = greenhouse;
   state.gauge = gauge;
   state.sunburst2 = sunburst2;
   state.line = line;
@@ -50,6 +55,7 @@ function init(){
   test = new Test(state, setGlobalState);
   bubble = new Bubble(state, setGlobalState);
   sunburst = new Sunburst(state, setGlobalState);
+  heatmap = new Heatmap(state, setGlobalState);
   gauge = new Gauge(state, setGlobalState);
   sunburst2 = new Sunburst2(state, setGlobalState);
   line = new Line(state, setGlobalState);
@@ -63,6 +69,7 @@ function draw() {
   test.draw(state, setGlobalState);
   bubble.draw(state, setGlobalState);
   sunburst.draw(state, setGlobalState);
+  heatmap.draw(state, setGlobalState);
   gauge.draw(state, setGlobalState);
   sunburst2.draw(state, setGlobalState);
   line.draw(state, setGlobalState);
