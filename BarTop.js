e6a6cf476d1bf972d315241917704dda5a23d1a2
kathiavf16 <kathia.vargasfeliz@gmail.com>
2021-12-25 21:12:14 -0500

    layout progress

--- BarTop.js
+++ BarTop.js
@@ -1,98 +1,75 @@
-class BarTop {
+class BarTop{
 
     constructor(state, setGlobalState) {
 
         // Code goes here
-       // set the dimensions and margins of the graph
-       // set the dimensions and margins of the graph
-       const margin = {
-               top: 10,
-               right: 30,
-               bottom: 70,
-               left: 50
-           },
-           width = 760 - margin.left - margin.right,
-           height = 400 - margin.top - margin.bottom;
+// set the dimensions and margins of the graph
+const margin = {
+        top: 10,
+        right: 30,
+        bottom: 90,
+        left: 40
+    },
+    width = 460 - margin.left - margin.right,
+    height = 450 - margin.top - margin.bottom;
 
-       // append the svg object to the body of the page
-       const svg = d3.select("#bartop")
-           .append("svg")
-           .attr("width", width + margin.left + margin.right)
-           .attr("height", height + margin.top + margin.bottom)
-           .append("g")
-           .attr("transform", `translate(${margin.left},${margin.top})`);
+// append the svg object to the body of the page
+const svg = d3.select("#bartop")
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", `translate(${margin.left},${margin.top})`);
 
+// Parse the Data
+d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/7_OneCatOneNum_header.csv").then(function (data) {
 
-       // Parse the Data
-       d3.csv("https://raw.githubusercontent.com/kathiavf16/ms_final/main/data/top.csv").then(function (data) {
+    // X axis
+    const x = d3.scaleBand()
+        .range([0, width])
+        .domain(data.map(d => d.Country))
+        .padding(0.2);
+    svg.append("g")
+        .attr("transform", `translate(0,${height})`)
+        .call(d3.axisBottom(x))
+        .selectAll("text")
+        .attr("transform", "translate(-10,0)rotate(-45)")
+        .style("text-anchor", "end");
 
-           // List of subgroups = header of the csv files = soil condition here
-           const subgroups = data.columns.slice(1)
+    // Add Y axis
+    const y = d3.scaleLinear()
+        .domain([0, 13000])
+        .range([height, 0]);
+    svg.append("g")
+        .call(d3.axisLeft(y));
 
-           // List of groups = species here = value of the first column called group -> I show them on the X axis
-           const groups = data.map(d => d.country)
+    // Bars
+    svg.selectAll("mybar")
+        .data(data)
+        .join("rect")
+        .attr("x", d => x(d.Country))
+        .attr("width", x.bandwidth())
+        .attr("fill", "#69b3a2")
+        // no bar at the beginning thus:
+        .attr("height", d => height - y(0)) // always equal to 0
+        .attr("y", d => y(0))
 
-           console.log("countries",groups, subgroups)
+    // Animation
+    svg.selectAll("rect")
+        .transition()
+        .duration(800)
+        .attr("y", d => y(d.Value))
+        .attr("height", d => height - y(d.Value))
+        .delay((d, i) => {
+            console.log(i);
+            return i * 100
+        })
 
-           // Add X axis
-           const x = d3.scaleBand()
-               .domain(groups)
-               .range([0, width])
-               .padding([0.2])
-           svg.append("g")
-               .attr("transform", `translate(0, ${height})`)
-               .call(d3.axisBottom(x).tickSize(0))
-               .selectAll("text")
-               .attr("transform", "translate(-10,0)rotate(-45)")
-               .style("text-anchor", "end");
+})
 
-           // Add Y axis
-           const y = d3.scaleLinear()
-               .domain([-5, 7])
-               .range([height, 0]);
-           svg.append("g")
-               .call(d3.axisLeft(y));
-
-           // Another scale for subgroup position?
-           const xSubgroup = d3.scaleBand()
-               .domain(subgroups)
-               .range([0, x.bandwidth()])
-               .padding([0.05])
-
-           // color palette = one color per subgroup
-           const color = d3.scaleOrdinal()
-               .domain(subgroups)
-               .range(['#e41a1c', '#377eb8', '#4daf4a'])
-
-           // Show the bars
-           svg.append("g")
-               .selectAll("g")
-               // Enter in data = loop group per group
-               .data(data)
-               .join("g")
-               .attr("transform", d => `translate(${x(d.country)}, 0)`)
-               .selectAll("rect")
-               .data(function (d) {
-                   return subgroups.map(function (key) {
-                       return {
-                           key: key,
-                           value: +d[key]
-                       };
-                   });
-               })
-               .join("rect")
-               .attr("x", d => xSubgroup(+d.key))
-               .attr("y", d => y(+d.value))
-               .attr("width", xSubgroup.bandwidth())
-               .attr("height", d => height - y(+d.value))
-               .attr("fill", d => color(d.key));
-
-       })
     } //end of constructor
 
     draw(state, setGlobalState) {
-
-
 
     }
 }
@@ -100,56 +77,3 @@ class BarTop {
 export {
     BarTop
 };
-
-// Add X axis
-/* const x = d3.scaleBand()
-    .domain(groups)
-    .range([0, width])
-    .padding([0.2])
-svg.append("g")
-    .attr("transform", `translate(0, ${height})`)
-    .call(d3.axisBottom(x).tickSize(0))
-    .selectAll("text")
-    .attr("transform", "translate(-10,0)rotate(-45)")
-    .style("text-anchor", "end");
-
-// Add Y axis
-const y = d3.scaleLinear()
-    .domain([-5, 7])
-    .range([height, 0]);
-svg.append("g")
-    .call(d3.axisLeft(y));
-
-// Another scale for subgroup position?
-const xSubgroup = d3.scaleBand()
-    .domain(subgroups)
-    .range([0, x.bandwidth()])
-    .padding([0.05])
-
-// color palette = one color per subgroup
-const color = d3.scaleOrdinal()
-    .domain(subgroups)
-    .range(['#e41a1c', '#377eb8', '#4daf4a'])
-
-// Show the bars
-svg.append("g")
-    .selectAll("g")
-    // Enter in data = loop group per group
-    .data(data)
-    .join("g")
-    .attr("transform", d => `translate(${x(d.country)}, 0)`)
-    .selectAll("rect")
-    .data(function (d) {
-        return subgroups.map(function (key) {
-            return {
-                key: key,
-                value: +d[key]
-            };
-        });
-    })
-    .join("rect")
-    .attr("x", d => xSubgroup(+d.key))
-    .attr("y", d => y(+d.value))
-    .attr("width", xSubgroup.bandwidth())
-    .attr("height", d => height - y(+d.value))
-    .attr("fill", d => color(d.key)); */
